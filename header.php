b04bb65f247d233364efa2d38c51a7bac459c797
Cristian Echeverri <cristian@dalerojo.net>
2015-08-01 10:01:55 -0500

    Revert "Article e imagen nueva"
    This reverts commit 225ceed7dbfb5a75deca79299960b5c6a2c70410.

--- header.php
+++ header.php
@@ -5,10 +5,6 @@
 	<title>Theme Wordpress</title>
 	<h1>Header nueva (mensaje editado</h1>
 	<h1>Segundo mensaje Header</h1>
-	<article>
-		<h1>Lunari murio de gonorrea</h1>
-		<img src="http://futbolete.com/wp-content/uploads/2015/03/lunari-4.jpg" alt="">
-	</article>
 </head>
 <body>
 	
