f4c82ffa3f05f90efd5d7f8ad94f77473f660542
Cristian Echeverri <cristian@dalerojo.net>
2015-08-01 11:04:15 -0500

    cambie el titulo

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Theme Wordpress</title>
+	<title>Seleccion colombia</title>
 	<h1>Header nueva (mensaje editado</h1>
 	<h1>Segundo mensaje Header</h1>
 	<article>
